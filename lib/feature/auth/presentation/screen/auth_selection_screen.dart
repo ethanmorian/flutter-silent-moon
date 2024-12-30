18c60bbf54e3a0019d110b5612283ea5b8f0ca79
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-12-30 20:04:50 +0900

    refactor: refactor AuthSelectionScreen widget to improve readability and maintainability

--- lib/feature/auth/presentation/screen/auth_selection_screen.dart
+++ lib/feature/auth/presentation/screen/auth_selection_screen.dart
@@ -11,124 +11,141 @@ class AuthSelectionScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            top: 0,
-            child: SvgPicture.asset(
-              'assets/auth_selection_background.svg',
-              width: 423.w,
-              height: 504.h,
+          _buildBackground(),
+          _buildLogo(),
+          _buildIllustration(),
+          _buildContent(),
+          _buildButtons(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBackground() {
+    return Positioned(
+      top: 0,
+      child: SvgPicture.asset(
+        'assets/auth_selection_background.svg',
+        width: 423.w,
+        height: 504.h,
+      ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Positioned(
+      top: 50.h,
+      left: 0,
+      right: 0,
+      child: SvgPicture.asset(
+        'assets/auth_selection_logo.svg',
+        width: 168.w,
+        height: 30.h,
+      ),
+    );
+  }
+
+  Widget _buildIllustration() {
+    return Positioned(
+      top: 160.h,
+      left: 0,
+      right: 0,
+      child: SvgPicture.asset(
+        'assets/auth_selection_image.svg',
+        width: 332.22.w,
+        height: 242.69.h,
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    return Positioned(
+      top: 534.h,
+      left: 0,
+      right: 0,
+      child: Column(
+        children: [
+          Text(
+            'We are what we do',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30.r,
+              color: Color(0xFF3F414E),
             ),
           ),
-          Positioned(
-            top: 50.h,
-            left: 0.w,
-            right: 0.w,
-            child: SvgPicture.asset(
-              'assets/auth_selection_logo.svg',
-              width: 168.w,
-              height: 30.h,
+          SizedBox(height: 15.h),
+          Text(
+            'Thousand of people are using silent moon\nfor smalls meditation',
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: 16.r,
+              color: Color(0xFFA1A4B2),
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons(BuildContext context) {
+    return Positioned(
+      top: 705.h,
+      left: 0,
+      right: 0,
+      child: Column(
+        children: [
+          TextButton(
+            onPressed: () => context.pushNamed('signUp'),
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(
+                Color(0xFF8E97FD),
+              ),
+              padding: WidgetStatePropertyAll(
+                EdgeInsets.symmetric(
+                  horizontal: 156.w,
+                  vertical: 25.h,
+                ),
+              ),
+              shape: WidgetStatePropertyAll(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(38.r),
+                ),
+              ),
+            ),
+            child: Text(
+              'SIGN UP',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 14.r,
+                color: Color(0xFFF6F1FB),
+              ),
             ),
           ),
-          Positioned(
-              top: 160.h,
-              left: 0.w,
-              right: 0.w,
-              child: SvgPicture.asset(
-                'assets/auth_selection_image.svg',
-                width: 332.22.w,
-                height: 242.69.h,
-              )),
-          Positioned(
-            top: 534.h,
-            left: 0.w,
-            right: 0.w,
-            child: Column(
-              children: [
-                Text(
-                  'We are what we do',
+          SizedBox(height: 20.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'ALREADY HAVE AN ACCOUNT? ',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14.r,
+                  color: Color(0xFFA1A4B2),
+                ),
+              ),
+              InkWell(
+                onTap: () => context.pushNamed('signIn'),
+                child: Text(
+                  'LOG IN',
                   style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30.r,
-                    color: Color(0xFF3F414E),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14.r,
+                    color: Color(0xFF8E97FD),
                   ),
                 ),
-                SizedBox(height: 15.h),
-                Text(
-                  'Thousand of people are usign silent moon\nfor smalls meditation',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 16.r,
-                    color: Color(0xFFA1A4B2),
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 705.h,
-            left: 0.w,
-            right: 0.w,
-            child: Column(
-              children: [
-                TextButton(
-                  onPressed: () {
-                    context.pushNamed('signUp');
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(
-                      Color(0xFF8E97FD),
-                    ),
-                    padding: WidgetStateProperty.all(
-                      EdgeInsets.symmetric(
-                        horizontal: 156.w,
-                        vertical: 25.h,
-                      ),
-                    ),
-                    shape: WidgetStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(38.r),
-                      ),
-                    ),
-                  ),
-                  child: Text(
-                    'SIGN UP',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 14.r,
-                      color: Color(0xFFF6F1FB),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20.h),
-                InkWell(
-                  onTap: () {
-                    context.pushNamed('signIn');
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'ALREADY HAVE AN ACCOUNT? ',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.r,
-                          color: Color(0xFFA1A4B2),
-                        ),
-                      ),
-                      Text(
-                        'LOG IN',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.r,
-                          color: Color(0xFF8E97FD),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
