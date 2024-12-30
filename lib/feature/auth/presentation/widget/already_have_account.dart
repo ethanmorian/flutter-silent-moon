921f5e238cf02a1428713102512955b73046902d
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-12-30 22:34:31 +0900

    feat: Add `AlreadyHaveAccount` widget for account navigation toggle

---
+++ lib/feature/auth/presentation/widget/already_have_account.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+
+class AlreadyHaveAccount extends StatelessWidget {
+  final bool alreadyHaveAccount;
+
+  const AlreadyHaveAccount({
+    required this.alreadyHaveAccount,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          alreadyHaveAccount
+              ? 'ALREADY HAVE AN ACCOUNT? '
+              : "DON'T HAVE AN ACCOUNT? ",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 14.r,
+            color: Color(0xFFA1A4B2),
+          ),
+        ),
+        InkWell(
+          onTap: () {
+            final route = alreadyHaveAccount ? 'signIn' : 'signUp';
+            context.pushNamed(route);
+          },
+          child: Text(
+            alreadyHaveAccount ? 'LOG IN' : 'SIGN UP',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 14.r,
+              color: Color(0xFF8E97FD),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
