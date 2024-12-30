7c2d536a82981ea0a87952e7c0d31be8a65a7790
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-12-30 22:34:59 +0900

    refactor: Refactor AuthSelectionScreen to use reusable widgets

--- lib/feature/auth/presentation/screen/auth_selection_screen.dart
+++ lib/feature/auth/presentation/screen/auth_selection_screen.dart
@@ -3,6 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../../core/widget/button_with_text.dart';
+import '../widget/already_have_account.dart';
+
 class AuthSelectionScreen extends StatelessWidget {
   const AuthSelectionScreen({super.key});
 
@@ -12,10 +15,26 @@ class AuthSelectionScreen extends StatelessWidget {
       body: Stack(
         children: [
           _buildBackground(),
-          _buildLogo(),
-          _buildIllustration(),
-          _buildContent(),
-          _buildButtons(context),
+          Positioned(
+            top: 50.h,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                _buildLogo(),
+                SizedBox(height: 80.h),
+                _buildImage(),
+                SizedBox(height: 131.31.h),
+                _buildMainText(),
+                SizedBox(height: 15.h),
+                _buildSubText(),
+                SizedBox(height: 62.h),
+                _buildSignUpButton(context),
+                SizedBox(height: 20.h),
+                _buildAlreadyHaveAccount(context),
+              ],
+            ),
+          )
         ],
       ),
     );
@@ -33,122 +52,54 @@ class AuthSelectionScreen extends StatelessWidget {
   }
 
   Widget _buildLogo() {
-    return Positioned(
-      top: 50.h,
-      left: 0,
-      right: 0,
-      child: SvgPicture.asset(
-        'assets/auth_selection_logo.svg',
-        width: 168.w,
-        height: 30.h,
+    return SvgPicture.asset(
+      'assets/auth_selection_logo.svg',
+      width: 168.w,
+      height: 30.h,
+    );
+  }
+
+  Widget _buildImage() {
+    return SvgPicture.asset(
+      'assets/auth_selection_image.svg',
+      width: 332.22.w,
+      height: 242.69.h,
+    );
+  }
+
+  Widget _buildMainText() {
+    return Text(
+      'We are what we do',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 30.r,
+        color: Color(0xFF3F414E),
       ),
     );
   }
 
-  Widget _buildIllustration() {
-    return Positioned(
-      top: 160.h,
-      left: 0,
-      right: 0,
-      child: SvgPicture.asset(
-        'assets/auth_selection_image.svg',
-        width: 332.22.w,
-        height: 242.69.h,
+  Widget _buildSubText() {
+    return Text(
+      'Thousand of people are using silent moon\nfor smalls meditation',
+      style: TextStyle(
+        fontWeight: FontWeight.w300,
+        fontSize: 16.r,
+        color: Color(0xFFA1A4B2),
       ),
+      textAlign: TextAlign.center,
     );
   }
 
-  Widget _buildContent() {
-    return Positioned(
-      top: 534.h,
-      left: 0,
-      right: 0,
-      child: Column(
-        children: [
-          Text(
-            'We are what we do',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30.r,
-              color: Color(0xFF3F414E),
-            ),
-          ),
-          SizedBox(height: 15.h),
-          Text(
-            'Thousand of people are using silent moon\nfor smalls meditation',
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-              fontSize: 16.r,
-              color: Color(0xFFA1A4B2),
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
+  Widget _buildSignUpButton(BuildContext context) {
+    return ButtonWithText(
+      onPressed: () => context.pushNamed('signUp'),
+      buttonText: 'SIGN UP',
     );
   }
 
-  Widget _buildButtons(BuildContext context) {
-    return Positioned(
-      top: 705.h,
-      left: 0,
-      right: 0,
-      child: Column(
-        children: [
-          TextButton(
-            onPressed: () => context.pushNamed('signUp'),
-            style: ButtonStyle(
-              backgroundColor: WidgetStatePropertyAll(
-                Color(0xFF8E97FD),
-              ),
-              padding: WidgetStatePropertyAll(
-                EdgeInsets.symmetric(
-                  horizontal: 156.w,
-                  vertical: 25.h,
-                ),
-              ),
-              shape: WidgetStatePropertyAll(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(38.r),
-                ),
-              ),
-            ),
-            child: Text(
-              'SIGN UP',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 14.r,
-                color: Color(0xFFF6F1FB),
-              ),
-            ),
-          ),
-          SizedBox(height: 20.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'ALREADY HAVE AN ACCOUNT? ',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 14.r,
-                  color: Color(0xFFA1A4B2),
-                ),
-              ),
-              InkWell(
-                onTap: () => context.pushNamed('signIn'),
-                child: Text(
-                  'LOG IN',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14.r,
-                    color: Color(0xFF8E97FD),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+  Widget _buildAlreadyHaveAccount(BuildContext context) {
+    return AlreadyHaveAccount(
+      alreadyHaveAccount: true,
     );
   }
 }
